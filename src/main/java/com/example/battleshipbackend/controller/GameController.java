a9101f92b0dd7aa4f53766389c34017733ac6c2a
Lukas Chyle <lukas_chyle@hotmail.com>
2025-05-08 20:14:01 +0200

    fixed a missing piece in GameController

--- src/main/java/com/example/battleshipbackend/controller/GameController.java
+++ src/main/java/com/example/battleshipbackend/controller/GameController.java
@@ -7,10 +7,12 @@ import com.example.battleshipbackend.statistics.service.GameStatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/api")
 public class GameController {
 
   private final GameStatisticsService gameStatisticsService;
